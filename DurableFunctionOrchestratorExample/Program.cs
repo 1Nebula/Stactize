3adfe20a0c02dd187a77c7422deb44802c0ebca1
charloconnell <charloconnell93@gmail.com>
2024-01-24 15:09:27 +0200

    Converted to isolated worker model
    Converted example orchestrator function to run using the isolated worker model for function apps.

---
+++ DurableFunctionOrchestratorExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orchestrator.Core.Contracts;
+using Orchestrator.Services;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        // Use the IServiceCollection to configure your application and add services for dependency injection
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddTransient<IServiceBusService, ServiceBusService>();
+
+        // This will allow the orchestrator function to handle the friendly names of enum values in the SubscriptionEvent enum
+        services.Configure<JsonSerializerOptions>(options =>
+        {
+            options.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false));
+            options.PropertyNameCaseInsensitive = true;
+        });
+    })
+    .Build();
+
+host.Run();
