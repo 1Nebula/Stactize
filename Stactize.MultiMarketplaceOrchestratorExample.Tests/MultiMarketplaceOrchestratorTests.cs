47ff030f17481f995ae672c6212eab7049997681
Micheal Wheeler <michealw@nebula.co.za>
2025-10-27 10:15:25 +0200

    Add unit tests for Stripe event handling

--- Stactize.MultiMarketplaceOrchestratorExample.Tests/MultiMarketplaceOrchestratorTests.cs
+++ Stactize.MultiMarketplaceOrchestratorExample.Tests/MultiMarketplaceOrchestratorTests.cs
@@ -7,6 +7,7 @@ using Moq;
 using MultiMarketplaceOrchestratorExample;
 using MultiMarketplaceOrchestratorExample.AWS;
 using MultiMarketplaceOrchestratorExample.Azure;
+using MultiMarketplaceOrchestratorExample.Stripe;
 using Orchestrator.Core;
 using Orchestrator.Core.Contracts;
 using Orchestrator.Core.Models;
@@ -89,6 +90,35 @@ namespace Stactize.MultiMarketplaceOrchestratorExample.Tests
         {
             //Arrange
             var orchestrationActionModel = _fixture.Build<AwsOrchestrationActionModel>()
+                .With(x => x.Event, subscriptionEvent)
+                .Create();
+            var expectedResult = _fixture.Create<OrchestrationResultModel>();
+
+            var context = new Mock<TaskOrchestrationContext>();
+            context.Setup(x => x.GetInput<OrchestrationActionBaseModel>()).Returns(orchestrationActionModel);
+            context.Setup(x => x.CallActivityAsync<OrchestrationResultModel>(taskName, orchestrationActionModel, It.IsAny<TaskOptions>()))
+                .ReturnsAsync(expectedResult);
+
+            //Act
+            var act = () => _sut.RunOrchestrator(context.Object);
+
+            //Assert
+            await act.ShouldNotThrowAsync();
+            context.Verify(x => x.CallActivityAsync<OrchestrationResultModel>(taskName, orchestrationActionModel, It.IsAny<TaskOptions>()),
+                Times.Once);
+            context.Verify(x => x.CallActivityAsync(nameof(MultiMarketplaceOrchestrator.CompleteOrchestratorAction), expectedResult, It.IsAny<TaskOptions>()),
+                Times.Once);
+        }
+
+        [TestCase(SubscriptionEvent.Create, nameof(StripeCreateFunction.StripeCreate))]
+        [TestCase(SubscriptionEvent.Update, nameof(StripeUpdateFunction.StripeUpdate))]
+        [TestCase(SubscriptionEvent.Delete, nameof(StripeDeleteFunction.StripeDelete))]
+        [TestCase(SubscriptionEvent.Reinstate, nameof(StripeReinstateFunction.StripeReinstate))]
+        [TestCase(SubscriptionEvent.Suspend, nameof(StripeSuspendFunction.StripeSuspend))]
+        public async Task RunOrchestrator_WithStripeOrchestrationActionModel_Should_CallCompleteOrchestrator(SubscriptionEvent subscriptionEvent, string taskName)
+        {
+            //Arrange
+            var orchestrationActionModel = _fixture.Build<StripeOrchestrationActionModel>()
                 .With(x => x.Event, subscriptionEvent)
                 .Create();
             var expectedResult = _fixture.Create<OrchestrationResultModel>();
---
+++ Stactize.MultiMarketplaceOrchestratorExample.Tests/StripeFunctionTests.cs
@@ -0,0 +1,123 @@
+﻿using AutoFixture;
+using MultiMarketplaceOrchestratorExample.Stripe;
+using Orchestrator.Core;
+using Orchestrator.Core.Models;
+using Shouldly;
+
+namespace Stactize.MultiMarketplaceOrchestratorExample.Tests
+{
+    public class StripeFunctionTests
+    {
+        private readonly Fixture _fixture;
+
+        public StripeFunctionTests()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void AzureCreate_Should_ReturnOrchestrationResultModel()
+        {
+            //Arrange
+            var action = _fixture.Create<StripeOrchestrationActionModel>();
+
+            //Act
+            var result = StripeCreateFunction.StripeCreate(action);
+
+            //Assert
+            result.ShouldSatisfyAllConditions(
+                () => result.ShouldNotBeNull(),
+                () => result.ShouldBeOfType<OrchestrationResultModel>(),
+                () => result.OperationId.ShouldBe(action.OperationId),
+                () => result.ApplicationId.ShouldBe(action.ApplicationId),
+                () => result.SubscriptionId.ShouldBe(action.SubscriptionId),
+                () => result.TenantId.ShouldBe(action.TenantId),
+                () => result.State.ShouldBe(OrchestrationState.Succeeded)
+            );
+        }
+
+        [Test]
+        public void AzureUpdate_Should_ReturnOrchestrationResultModel()
+        {
+            //Arrange
+            var action = _fixture.Create<StripeOrchestrationActionModel>();
+
+            //Act
+            var result = StripeUpdateFunction.StripeUpdate(action);
+
+            //Assert
+            result.ShouldSatisfyAllConditions(
+                () => result.ShouldNotBeNull(),
+                () => result.ShouldBeOfType<OrchestrationResultModel>(),
+                () => result.ApplicationId.ShouldBe(action.ApplicationId),
+                () => result.OperationId.ShouldBe(action.OperationId),
+                () => result.SubscriptionId.ShouldBe(action.SubscriptionId),
+                () => result.TenantId.ShouldBe(action.TenantId),
+                () => result.State.ShouldBe(OrchestrationState.Succeeded)
+            );
+        }
+
+        [Test]
+        public void AzureSuspend_Should_ReturnOrchestrationResultModel()
+        {
+            //Arrange
+            var action = _fixture.Create<StripeOrchestrationActionModel>();
+
+            //Act
+            var result = StripeSuspendFunction.StripeSuspend(action);
+
+            //Assert
+            result.ShouldSatisfyAllConditions(
+                () => result.ShouldNotBeNull(),
+                () => result.ShouldBeOfType<OrchestrationResultModel>(),
+                () => result.ApplicationId.ShouldBe(action.ApplicationId),
+                () => result.OperationId.ShouldBe(action.OperationId),
+                () => result.SubscriptionId.ShouldBe(action.SubscriptionId),
+                () => result.TenantId.ShouldBe(action.TenantId),
+                () => result.State.ShouldBe(OrchestrationState.Succeeded)
+            );
+        }
+
+        [Test]
+        public void AzureReinstate_Should_ReturnOrchestrationResultModel()
+        {
+            //Arrange
+            var action = _fixture.Create<StripeOrchestrationActionModel>();
+
+            //Act
+            var result = StripeReinstateFunction.StripeReinstate(action);
+
+            //Assert
+            result.ShouldSatisfyAllConditions(
+                () => result.ShouldNotBeNull(),
+                () => result.ShouldBeOfType<OrchestrationResultModel>(),
+                () => result.ApplicationId.ShouldBe(action.ApplicationId),
+                () => result.OperationId.ShouldBe(action.OperationId),
+                () => result.SubscriptionId.ShouldBe(action.SubscriptionId),
+                () => result.TenantId.ShouldBe(action.TenantId),
+                () => result.State.ShouldBe(OrchestrationState.Succeeded)
+            );
+        }
+
+        [Test]
+        public void AzureDelete_Should_ReturnOrchestrationResultModel()
+        {
+            //Arrange
+            var action = _fixture.Create<StripeOrchestrationActionModel>();
+
+            //Act
+            var result = StripeDeleteFunction.StripeDelete(action);
+
+            //Assert
+            result.ShouldSatisfyAllConditions(
+                () => result.ShouldNotBeNull(),
+                () => result.ShouldBeOfType<OrchestrationResultModel>(),
+                () => result.ApplicationId.ShouldBe(action.ApplicationId),
+                () => result.OperationId.ShouldBe(action.OperationId),
+                () => result.SubscriptionId.ShouldBe(action.SubscriptionId),
+                () => result.TenantId.ShouldBe(action.TenantId),
+                () => result.State.ShouldBe(OrchestrationState.Succeeded)
+            );
+        }
+    }
+}
